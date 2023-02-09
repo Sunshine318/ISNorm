2b94074bfd217a9638e95f76755f49385182e154
Sunshine318 <47991098+Sunshine318@users.noreply.github.com>
2023-02-09 17:31:53 -0500

    added desc for getIS

--- R/getIS.R
+++ R/getIS.R
@@ -1,9 +1,21 @@
 ####function to get IS for full data set####
 ####y is classnames to be used########data is the data frame for data to be normalized
+#' Title
+#'
+#' @param y IS name
+#' @param data  dataframe to be normalized and has IS data in it for all samples
+#'
+#' @return extract IS data from data.frame
+#' @export
+#'
+#' @examples
 getIS<-function(y,data){
   ####removed log #######
   tmp<-t(data)
   classP_PC0<-tmp[,which(colnames(tmp)==y)]
   #classP_PC0<-tmp[,grep(y,colnames(tmp))]
+
+# Insert roxygen ----------------------------------------------------------
+
 
 }
